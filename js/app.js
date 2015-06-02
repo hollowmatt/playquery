3f63ac8acb9e387ceca8e14f291a27381239731e
Matthew Holloway <matthew.holloway@logicwerx.com>
2015-06-01 17:09:11 -0700

    add bouncy to the paragraph

--- js/app.js
+++ js/app.js
@@ -10,11 +10,11 @@
 			// $('#cats').toggle('slow');
 			if ($('#cats').is(':visible')) {
 				$('#cats').fadeOut(1000);
-				$('#hideButton').val("Show");	
+				$('#hideButton').val("Show");
 				$('#catstatus').text("Where are the current cats?");
 			} else {
 				$('#cats').fadeIn(1000);
-				$('#hideButton').val("Hide");	
+				$('#hideButton').val("Hide");
 				$('#catstatus').text("These are the current cats.");
 			}
 		});
@@ -26,8 +26,8 @@
 		});
 		$('#catinfo > div').hide();
 		$('#catinfo h3').click(function() {
-		  $(this).next().animate( 
-		    {'height':'toggle'}, 'slow', 'linear'
+		  $(this).next().animate(
+		    {'height':'toggle'}, 'slow', 'easeOutBounce'
 		  );
 		});
 	}
@@ -35,9 +35,8 @@
 	//Function to add alternating row color to a table
 	function alternateRows(tableID) {
 		console.log("Table to alternate: " + tableID);
-		$('#' + tableID + ' tbody tr:even').addClass('zebra');	
+		$('#' + tableID + ' tbody tr:even').addClass('zebra');
 	}
-
 
 	$(document).ready(initialize());
 })();
